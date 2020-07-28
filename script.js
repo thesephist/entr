76097f47ff447b6349eb4ef51ba39e48295cb3d9
Linus Lee <linus@thesephist.com>
2020-07-28 10:04:11 +0000

    UI Polish and data cleanup

--- script.js
+++ script.js
@@ -39,12 +39,18 @@ function noteMatches(search, { heading, lines }) {
     return true;
   }
 
+  search = search.toLowerCase();
+
+  if (heading && heading.toLowerCase().includes(search)) {
+    return true;
+  }
+
   if (lines.length === 1) {
-    return lines[0].toLowerCase().includes(search.toLowerCase());
+    return lines[0].toLowerCase().includes(search);
   }
 
   for (const line of lines) {
-    if (line.toLowerCase().includes(search.toLowerCase())) {
+    if (line.toLowerCase().includes(search)) {
       return true;
     }
   }
@@ -76,37 +82,47 @@ class App extends Component {
       this.notes = parseNotes(text);
       this.render();
     }).catch(e => {
-      // TODO: improve.
-      alert(e);
+      alert(`Couldn't load notes! The error is the following. Feel free to let Linus know :)`, e);
       console.log(e);
     });
   }
   compose() {
     const matched = this.notes.filter(note => noteMatches(this.search, note));
     return html`<div class="app">
-    <header class="accent paper">
-      Linus's (<a href="https://thesephist.com">@thesephist</p>) notes on startups and life.
-      <br/>
-      <strong>${matched.length ? `${matched.length} results` : 'loading notes...'}</strong>
-    </header>
-    <div class="searcher">
-      <input type="text"
-        class="paper paper-border-left search-input"
-        placeholder="Search Linus's notes..."
-        autofocus
-          oninput=${evt => {
+      <header class="accent paper">
+        <a href="https://thesephist.com" target="_blank">Linus's</a> notes on startups and life.
+        <br/>
+        <strong>${this.notes.length ? `${matched.length} results` : 'loading notes...'}</strong>
+      </header>
+      <div class="searcher">
+        <input type="text"
+          class="paper paper-border-left search-input"
+          placeholder="Search Linus's notes..."
+          autofocus
+            oninput=${evt => {
         this.search = evt.target.value;
         this.render();
       }} />
-      <button class="movable paper paper-border-right about-button">
-        about
-      </button>
-    </div>
-    <div class="results">
-      <ul class="note-list">
-        ${matched.map(Note)}
-      </ul>
-    </div>
+        <button class="movable paper paper-border-right about-button">
+          about
+        </button>
+      </div>
+      <div class="results ${this.notes.length ? '' : 'loading'}">
+        <ul class="note-list">
+          ${matched.map(Note)}
+        </ul>
+      </div>
+      <footer>
+        <p>
+          Made with <a href="https://github.com/thesephist/torus" target="_blank">Torus</a>
+          and
+          <a href="https://thesephist.github.io/paper.css/" target="_blank">paper.css</a>
+          by
+          <a href="https://thesephist.com/" target="_blank">Linus</a>
+          with inspiration from
+          <a href="https://sahilkapur.com/" target="_blank">Sahil</a>
+        </p>
+      </footer>
     </div>`;
   }
 }
